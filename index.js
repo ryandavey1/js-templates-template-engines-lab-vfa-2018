a9b8e6365533353243719efcd4774cea610883ca
Ryan Davey <ryancdavey@icloud.com>
2018-06-18 23:07:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 // index.js
+function createPost() {
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commentText").value;
+ 
+  var commentTemplate = document.getElementById("comment-template").innerHTML;
+ 
+  //create template function
+  var templateFn = _.template(commentTemplate);
+ 
+  var commentsDiv = document.getElementById("comments");
+ 
+  //execute template function with JSON object for the interpolated values
+  var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
+ 
+  //append rather than replace!
+  commentsDiv.innerHTML += templateHTML;
+}
+
 function postComment() {
   var commenter = document.getElementById("commenterName").value;
   var comment = document.getElementById("commentText").value;
